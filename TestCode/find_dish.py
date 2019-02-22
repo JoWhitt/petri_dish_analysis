c4dccc372025be3b2d56d2bf42f04668efb68e6e
Joanna Whittam <whittamj@tcd.ie>
2019-02-22 11:00:50 +0000

    tidied find_dish algorithm

--- TestCode/find_dish.py
+++ TestCode/find_dish.py
@@ -1,23 +1,30 @@
-import cv2
-import matplotlib
+import cv2 as cv
+# import matplotlib
 import numpy as np
+import argparse
 
-img = cv2.imread('dish.jpg',0)
-output = img.copy()
-img = cv2.medianBlur(img,5)
-cimg = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
-circles = cv2.HoughCircles(img,cv2.HOUGH_GRADIENT,1,100)
-                    
-					
+image_path = "..\\sample_p_dish_images\\green_dots.jpg"
+
+''' load the image, clone it for output, and then convert it to grayscale '''
+image = cv.imread(image_path)
+assert(image is not None) 
+
+output = image.copy()
+image = cv.medianBlur(image,5)
+gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+# cimg = cv.cvtColor(image,cv.COLOR_GRAY2BGR)
+
+''' apply hough algorithm to locate dish '''
+circles = cv.HoughCircles(gray,cv.HOUGH_GRADIENT,1,100)
+                    			
 if circles is not None:
 	#circles = np.uint16(np.around(circles))
 	circles = np.round(circles[0, :]).astype("int")
 	for (x, y, r) in circles:
-		# draw the circle in the output image, then draw a rectangle
-		# corresponding to the center of the circle
-		cv2.circle(output, (x, y), r, (0, 255, 0), 4)
-		cv2.rectangle(output, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
+		#  circle
+		cv.circle(output, (x, y), r, (0, 255, 0), 4)
+		cv.rectangle(output, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
  
 	# show the output image
-	cv2.imshow("output", np.hstack([img, output]))
-	cv2.waitKey(0)
+	cv.imshow("output", np.hstack([image, output]))
+	cv.waitKey(0)
