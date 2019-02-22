dc5acca898f9ca629a169b31eb65f82ff2ea2e6b
Joanna Whittam <whittamj@tcd.ie>
2019-02-22 12:23:59 +0000

    added masking to dish location

--- TestCode/find_dish.py
+++ TestCode/find_dish.py
@@ -1,30 +1,48 @@
 import cv2 as cv
-# import matplotlib
 import numpy as np
-import argparse
 
 image_path = "..\\sample_p_dish_images\\green_dots.jpg"
 
-''' load the image, clone it for output, and then convert it to grayscale '''
+# ----------------------------------------------------------------------------
+# ----------------------------------------------------------------------------
+
+def get_dish_mask(image, circle):
+	(x, y, r) = circle
+
+	mask = np.zeros(image.shape, np.uint8) # initialise image
+	cv.circle(mask, (x, y), int(r/2), (255,255,255), r+1) # draw circle into mask
+
+	return mask
+
+# ----------------------------------------------------------------------------
+# ----------------------------------------------------------------------------
+
+''' load the image, clone it for output, and convert to grayscale '''
 image = cv.imread(image_path)
-assert(image is not None) 
+assert(image is not None)  # image read successfully
 
 output = image.copy()
-image = cv.medianBlur(image,5)
-gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-# cimg = cv.cvtColor(image,cv.COLOR_GRAY2BGR)
+
+gray = cv.medianBlur(image,5)
+gray = cv.cvtColor(gray, cv.COLOR_BGR2GRAY)
 
 ''' apply hough algorithm to locate dish '''
 circles = cv.HoughCircles(gray,cv.HOUGH_GRADIENT,1,100)
-                    			
-if circles is not None:
-	#circles = np.uint16(np.around(circles))
-	circles = np.round(circles[0, :]).astype("int")
-	for (x, y, r) in circles:
-		#  circle
-		cv.circle(output, (x, y), r, (0, 255, 0), 4)
-		cv.rectangle(output, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
- 
-	# show the output image
-	cv.imshow("output", np.hstack([image, output]))
-	cv.waitKey(0)
+assert(circles is not None)  # dish found
+assert(len(circles)==1)  	 # only one dish found
+
+#circles = np.uint16(np.around(circles))
+circles = np.round(circles[0, :]).astype("int")
+
+# ''' draw circle into output image '''
+# (x, y, r) = circles[0]
+# cv.circle(output, (x, y), r, (0, 255, 0), 4)
+# cv.rectangle(output, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
+
+''' create & apply mask '''
+mask = get_dish_mask(image, circles[0])
+output = cv.bitwise_and(output, mask)
+
+# show the output image
+cv.imshow("output", np.hstack([image, output]))
+cv.waitKey(0)
